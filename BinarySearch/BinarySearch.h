230c06bda9c5bf4f80bb018010379ea5492d36a8
kly <kanglieyong@gmail.com>
2015-03-31 08:44:26 +0800

    BinarySearch algorithm

---
+++ BinarySearch/BinarySearch.h
@@ -0,0 +1,28 @@
+#ifndef BINARYSEARCH_H_
+#define BINARYSEARCH_H_
+
+#define NOT_FOUND -1
+#include <vector>
+using std::vector;
+
+template <typename Comparable>
+int binarySearch(const vector<Comparable>& a, const Comparable& x)
+{
+  int low = 0, high = a.size() - 1;
+
+  while (low <= high) {
+    int mid = (low + high) / 2;
+
+    if (x < a[mid]) {
+      high = mid - 1;
+    } else if (a[mid] < x) {
+      low = mid + 1;
+    } else {
+      return mid;  // Found
+    }
+  }
+
+  return NOT_FOUND;
+}
+
+#endif
---
+++ BinarySearch/testBinarySearch.cpp
@@ -0,0 +1,26 @@
+#include "BinarySearch.h"
+#include <iostream>
+#include <vector>
+
+int main()
+{
+  using std::cout;
+  using std::endl;
+  using std::vector;
+  
+  vector<int> aVec({2, 12, 31, 35, 55, 89, 111, 210, 291, 400});
+  for (auto & x : aVec) {
+    cout << x << " ";
+  }
+  cout << endl;
+
+  int ta = binarySearch(aVec, 15);
+  int tb = binarySearch(aVec, 35);
+  int tc = binarySearch(aVec, 500);
+
+  cout << "ta = " << ta << endl
+       << "bt = " << tb << endl
+       << "tc = " << tc << endl;
+
+  return 0;
+}
