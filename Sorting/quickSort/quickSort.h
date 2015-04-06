50d4cbba8538a47fe878bb26fa5aca51be84e79f
kly <kanglieyong@gmail.com>
2015-04-07 03:57:44 +0800

    some changes

--- Sorting/quickSort/quickSort.h
+++ Sorting/quickSort/quickSort.h
@@ -2,10 +2,18 @@
 
 using std::vector;
 
-// declarition
+// Declarition
+template <typename Comparable>
 void swap(Comparable& a, Comparable& b);
 
+template <typename Comparable>
+const Comparable& median3(vector<Comparable>& a, int left, int right);
 
+template <typename Comparable>
+void quickSort(vector<Comparable>& a, int left, int right);
+
+
+// Definition
 template <typename Comparable>
 void quickSort(vector<Comparable>& a)
 {
@@ -13,10 +21,50 @@ void quickSort(vector<Comparable>& a)
 }
 
 template <typename Comparable>
+void quickSort(vector<Comparable>& a, int left, int right)
+{
+  if (left + 10 <= right) {
+    Comparable pivot = median3(a, left, right);
+
+    // Begin partioning
+    int i = left, j = right - 1;
+    for (; ; ) {
+      while (a[++i] pivot) {}
+      while (pivot < a[--j]) {}
+      if (i < j) swap(a[i], a[j]);
+      else break;
+    }
+
+    swap(a[i], a[right - 1]);    // Restore pivot
+
+    quickSort(a, left, i - 1);   // Sort small elements
+    quickSort(a, i + 1, right);  // Sort large elements
+  }
+  // Do an insertion sort on the subarray
+  else {
+    insertionSort(a, left, right);
+
+    int i = left + 1, j = right - 2;
+    for (; ; ) {
+      while (a[i] < pivot) i++;
+      while (pivot < a[j]) j--;
+      if (i < j) swap(a[i], a[j]);
+      else break;
+    }
+  }
+}
+
+template <typename Comparable>
 const Comparable& median3(vector<Comparable>& a, int left, int right)
 {
   int center = (left + right) / 2;
-  if (a[center] < a[left]) swap(a[left], a[center]);
+  if (a[center] < a[left] )   swap(a[left],   a[center]);
+  if (a[right]  < a[left] )   swap(a[left],   a[right]);
+  if (a[right]  < a[center] ) swap(a[center], a[right]);
+
+  // Place pivot at position right - 1
+  swap(a[center], a[right - 1]);
+  return a[right - 1];
 }
 
 template <typename Comparable>
@@ -26,4 +74,21 @@ void swap(Comparable& a, Comparable& b)
   tmp = a;
   a = b;
   b = tmp;
+}
+/*
+ * Simple insertion sort.
+ */
+
+template <typename Comparable>
+void insertionSort(vector<Comparable>& a, int left, int right)
+{
+  int j;
+
+  for (int p = left + 1; p < right; p++) {
+    Comparable tmp = a[p];
+    for (j = p; j > 0 && tmp < a[j - 1]; j--) {
+      a[j] = a[j - 1];
+    }
+    a[j] = tmp;
+  }
 }
