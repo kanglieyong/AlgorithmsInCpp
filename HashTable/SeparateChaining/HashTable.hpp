c3b8931fbe5600accfc44313cf4db8c8cd1e1938
kanglieyong <kanglieyong@gmail.com>
2015-04-23 22:25:47 +0800

    HashTable

---
+++ HashTable/SeparateChaining/HashTable.hpp
@@ -0,0 +1,10 @@
+#include <vector>
+#include <list>
+
+using std::vector;
+using std::list;
+
+template <typename HashedObj>
+class HashTable
+{
+};
---
+++ HashTable/TestHash/hash.h
@@ -0,0 +1,31 @@
+#include <string>
+
+using std::string;
+
+int hash1(const string& key, int tableSize)
+{
+  int hashVal = 0;
+
+  for (int i = 0; i < key.length(); i++) {
+    hashVal += key[i];
+  }
+
+  return hashVal % tableSize;
+}
+
+int hash2(const string& key, int tableSize)
+{
+  return (key[0] + 27 * key[1] + 729 * key[2]) % tableSize;
+}
+
+int hash3(const string& key, int tableSize)
+{
+  int hashVal = 0;
+
+  for (int i = 0; i < key.length(); i++) hashVal = 37 * hashVal + key[i];
+
+  hashVal %= tableSize;
+  if (hashVal < 0) hashVal += tableSize;
+
+  return hashVal;
+}
