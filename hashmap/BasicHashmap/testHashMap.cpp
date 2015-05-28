a8e486afc6df0304ade6ced70de36bed93877384
kanglieyong <kanglieyong@gmail.com>
2015-05-28 22:54:07 +0800

    TEST FILE

---
+++ hashmap/BasicHashmap/testHashMap.cpp
@@ -0,0 +1,35 @@
+#include "hashmap.h"
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void)
+{
+  DefaultHash<int> a;
+  DefaultHash<string> b;
+  cout << a.hash(23) << endl;
+  cout << b.hash("Hello") << endl;
+
+  hashmap<int, int> myHash;
+  myHash.insert(make_pair(4, 40));
+  myHash.insert(make_pair(6, 60));
+
+  auto x = myHash.find(4);
+  if (x != nullptr) cout << "4 maps to " << x->second << endl;
+  else              cout << "cannot find 4 in map" << endl;
+
+  myHash.erase(4);
+  auto x2 = myHash.find(4);
+  if (x2 != nullptr) cout << "4 maps to " << x2->second << endl;
+  else               cout << "cannot find 4 in map" << endl;
+
+  myHash[4] = 23;
+  myHash[4] = 60;
+
+  auto x3 = myHash.find(4);
+  if (x3 != nullptr) cout << "4 maps to " << x3->second << endl;
+  else               cout << "cannot find 4 in map" << endl;
+
+  return 0;
+}
