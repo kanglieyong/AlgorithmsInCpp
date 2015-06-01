fa2da5af8fc8a08f8fc398a54a962599ae006e3d
kanglieyong <kanglieyong@gmail.com>
2015-06-01 18:05:44 +0800

    unique the element in the vector

--- wordMap/testAdjacentWords.cpp
+++ wordMap/testAdjacentWords.cpp
@@ -1,8 +1,19 @@
 #include "headers.h"
+#include <algorithm>
+#include <set>
+#include <iterator>
+
+using std::function;
+using std::set;
+using std::back_insert_iterator;
 
 int main(void)
 {
-  vector<string> a({"helo", "hero", "five", "sive", "tive", "gear", "geat", "gelo", "selo", "sivt", "moet", "moto"});
+  vector<string> a({"helo", "hero", "five", "sive", "tive", "five", "gear", "geat", "gelo", "selo", "hero",  "sivt", "moet", "moto"});
+  set<string> wordset(a.begin(), a.end());
+  a.clear();
+  copy(wordset.begin(), wordset.end(), back_insert_iterator<vector<string> >(a));
+
   map<string, vector<string> > b = computeAdjacentWords3(a);
 
   printHighChangeables(b, 2);
